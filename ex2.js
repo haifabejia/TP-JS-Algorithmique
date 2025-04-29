8f196df9d33f62caedb55f7554fd8b460d6ba010
haifabejia <hayfa.bejia@etu.unistra.fr>
2025-04-29 16:23:49 +0200

    Update ex2.js

--- ex2.js
+++ ex2.js
@@ -1,16 +1,19 @@
-let montant = 369;
-let billets = [200, 100, 50, 20, 10, 5, 2, 1];
-let renduMonnaie = {};
+function rendreLaMonnaie(montant) {
+    const billets = [200, 100, 50, 20, 10, 5, 2, 1];
+    const rendu = {};
 
-billets.forEach(function(item) {
-    let nbBillets = Math.floor(montant / item);
-    if (nbBillets > 0) {
-        renduMonnaie[item] = nbBillets;
-        montant -= nbBillets * item;
+    billets.forEach(function(billet) {
+        let nbBillets = Math.floor(montant / billet);
+        if (nbBillets > 0) {
+            rendu[billet] = nbBillets;
+            montant -= nbBillets * billet;
+        }
+    });
+
+    console.log("Monnaie à rendre :");
+    for (let billet in rendu) {
+        console.log('- ${rendu[billet]} x ${billet}€');
     }
-});
-
-console.log("Monnaie rendue :");
-for (let billet in renduMonnaie) {
-    console.log('${renduMonnaie[billet]} x ${billet}€');
 }
+
+rendreLaMonnaie(369);
