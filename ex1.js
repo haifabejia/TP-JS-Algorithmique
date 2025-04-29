03a18295542c9883eeabdaa5a93c1fd0e2ff9255
haifabejia <hayfa.bejia@etu.unistra.fr>
2025-04-29 16:26:12 +0200

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -1,11 +1,18 @@
-for (let A = 1; A <= 9; A++) {
-    for (let B = 1; B <= 9; B++) {
-        for (let C = 1; C <= 9; C++) {
-            let nombre = A * 100 + B * 10 + C;
-            let sommeDesCubes = Math.pow(A, 3) + Math.pow(B, 3) + Math.pow(C, 3);
-            if (nombre == sommeDesCubes) {
-                console.log('Solution trouvée : ${A}${B}${C}');
+function trouverNombresArmstrong() {
+    let compteur = 0;
+    for (let A = 1; A <= 9; A++) {
+        for (let B = 1; B <= 9; B++) {
+            for (let C = 1; C <= 9; C++) {
+                let nombre = A * 100 + B * 10 + C;
+                let sommeCubes = (A * A * A) + (B * B * B) + (C * C * C);
+                if (nombre == sommeCubes) {
+                    console.log('Trouvé : ${nombre} = ${A}³ + ${B}³ + ${C}³');
+                    compteur++;
+                }
             }
         }
     }
+    console.log('Nombre total de solutions trouvées : ${compteur}');
 }
+
+trouverNombresArmstrong();
