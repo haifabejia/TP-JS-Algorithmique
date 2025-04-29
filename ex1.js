9c5a4a3e9193b8e443b30309a1fab07a4cc4b885
haifabejia <hayfa.bejia@etu.unistra.fr>
2025-04-29 16:09:02 +0200

    Create ex1.js

---
+++ ex1.js
@@ -0,0 +1,11 @@
+for (let A = 1; A <= 9; A++) {
+    for (let B = 1; B <= 9; B++) {
+        for (let C = 1; C <= 9; C++) {
+            let nombre = A * 100 + B * 10 + C;
+            let sommeDesCubes = Math.pow(A, 3) + Math.pow(B, 3) + Math.pow(C, 3);
+            if (nombre === sommeDesCubes) {
+                console.log(`Solution trouvée : ${A}${B}${C}`);
+            }
+        }
+    }
+}
